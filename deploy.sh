3ba990bef1495aef03c4196e6807afd6ca7a5c30
Nicolas Pagny <nicolas.pagny.pro@gmail.com>
2024-10-15 09:11:13 +0200

    modification de la branche à utiliser pour git pull

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Récupérer les dernières modifications de la branche main
 echo "Mise à jour du dépôt Git..."
-if git pull origin feature/api_sans_docker --no-rebase; then
+if git pull origin main --no-rebase; then
     echo "Mise à jour réussie."
 else
     echo "Echec de la mise à jour depuis GitHub. Déploiement annulé."
