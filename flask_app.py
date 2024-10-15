a85de002404091be3a87db3d8e277467dbc2e046
Nicolas Pagny <nicolas.pagny.pro@gmail.com>
2024-10-15 11:53:25 +0200

    v3

--- flask_app.py
+++ flask_app.py
@@ -33,7 +33,7 @@ tracer = trace.get_tracer(__name__)
 # Simple Hello world pour vérifier si le déploiement automatiquement a bien mis à jour la nouvelle version
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask! Reload v2!'
+    return 'Hello from Flask! Reload v3!'
 
 # Api de prédiction
 @app.route("/predict", methods=["GET"])
